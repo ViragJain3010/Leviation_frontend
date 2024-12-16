929d9dafafa15807eafab82d1dc59fcaf35e4d05
ViragJain3010 <viragjain3010@gmail.com>
2024-12-16 16:42:13 +0530

    updated route

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/" element={<Navigate to="/signup" />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route 
